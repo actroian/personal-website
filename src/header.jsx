722208446a35b06c136ba66aa768f6cb6c903f21
Adam Troiani <actroian@uwaterloo.ca>
2023-08-13 22:16:46 -0400

    Corrected Logo Size and Styling Header

--- src/header.jsx
+++ src/header.jsx
@@ -5,7 +5,7 @@ import './index.css'
 function SubHeader() {
   return (
     <>
-      <p>placeholder</p>
+      <div class=" text-white text-lg hover:text-green-600 hover:bg-slate-400 hover:outline-dotted hover:outline-red-400 no-underline h-min w-min align-middle"/>
     </>
   )
 }
@@ -13,16 +13,16 @@ function SubHeader() {
 function Header() {
   return (
     <>
-      <div class="bg-amber-200" >
-        <div class="subHeader">
-          <div class="indexLink">
-            <a class="nodecoration" href ="index.html" id="mainLogo" >
+      <div class="bg-amber-300 h-min w-full absolute top-0 left-0 rounded-md">
+        <div>
+          <div class="left-1">
+            <a href ="index.html" id="mainLogo" >
               <embed type="image/jpg" src="./src/assets/logo.jpg"/>
               <h2>Adam Troiani</h2>
             </a>
           </div>
-          <div class="webpageLinks">
-            <a class="nodecoration">
+          <div class="grid gap-4 grid-cols-3 grid-rows-1">
+            <a>
               <h2>Projects</h2>
               <h2>Resume</h2>
               <h2>Contact</h2>
@@ -30,10 +30,10 @@ function Header() {
           </div>
           <div class="externalLinks">
             <a class="nodecoration" href="https://www.linkedin.com/in/adamtroiani" target="_blank">
-              <img src="./src/assets/linkedin.png"/>
+              <img class="w-10 h-10" src="./src/assets/linkedin.png"/>
             </a>
             <a class="nodecoration" href="https://github.com/actroian" target="_blank">
-              <img src="./src/assets/github.png"/>
+              <img class="w-10 h-10" src="./src/assets/github.png"/>
             </a>
           </div>
         </div>
