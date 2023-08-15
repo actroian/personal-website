36a7bc26be76b2e1ed16a6aa642036793d113a5a
Adam Troiani <adam.troiani@windriver.com>
2023-08-15 13:58:38 -0400

    Removed unnecessary divs

--- src/header.jsx
+++ src/header.jsx
@@ -12,27 +12,25 @@ function SubHeader() {
 function Header() {
   return (
     <>
-      <div class="bg-amber-300 h-min w-full absolute top-0 left-0 rounded-md flow-root">
-        <div class="h-min w-min whitespace-nowrap relative">
-          <div class="h-min w-min bg-yellow-500">
-            <a href ="index.html">
-              <embed type="image/jpg" src="./logo.jpg"/>
-              <h2>Adam Troiani</h2>
-            </a>
-          </div>
-          <div class="bg-red-500">
-            <a class="pl-2 pr-2">Projects</a>
-            <a class="pl-2 pr-2">Resume</a>
-            <a class="pl-2 pr-2">Contact</a>
-          </div>
-          <div class="w-10 h-10 bg-blue-500 float-right">
-            <a href="https://github.com/actroian" target="_blank">
-              <img src="./github.png"/>
-            </a>
-            <a href="https://www.linkedin.com/in/adamtroiani" target="_blank">
-              <img src="./linkedin.png"/>
-            </a>
-          </div>
+      <div class="bg-amber-300 h-min w-full absolute top-0 left-0 rounded-md whitespace-nowrap flex">
+        <div class="bg-yellow-500 h-min w-min p-4"> 
+          <a href ="index.html">
+            <embed type="image/jpg" src="./logo.jpg"/>
+            <h2>Adam Troiani</h2>
+          </a>
+        </div>
+        <ul class="bg-red-500 h-min w-min text-center">
+          <li><a class="pl-2 pr-2">Projects</a></li>
+          <li><a class="pl-2 pr-2">Resumé</a></li>
+          <li><a class="pl-2 pr-2">Contact</a></li>
+        </ul>
+        <div class="w-10 h-10 bg-blue-500">
+          <a href="https://github.com/actroian" target="_blank">
+            <img src="./github.png"/>
+          </a>
+          <a href="https://www.linkedin.com/in/adamtroiani" target="_blank">
+            <img src="./linkedin.png"/>
+          </a>
         </div>
       </div>
     </>
