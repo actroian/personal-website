3fd52c11ba8f2f30cd5632765f66ce3cfe4a44c7
Adam Troiani <adam.troiani@windriver.com>
2023-08-21 13:30:01 -0400

    Subheader Components working properly

--- src/header.jsx
+++ src/header.jsx
@@ -13,23 +13,23 @@ function Header() {
   return (
     <>
       <div class="bg-amber-300 h-min w-full absolute top-0 left-0 rounded-md whitespace-nowrap flex">
-        <div class="bg-yellow-500 h-min w-min p-4"> 
+        <div class="bg-yellow-500 h-min w-min p-4 flex"> 
           <a href ="index.html">
             <embed type="image/jpg" src="./logo.jpg"/>
             <h2>Adam Troiani</h2>
           </a>
         </div>
-        <ul class="bg-red-500 h-min w-min text-center">
-          <li><a class="pl-2 pr-2">Projects</a></li>
-          <li><a class="pl-2 pr-2">Resumé</a></li>
-          <li><a class="pl-2 pr-2">Contact</a></li>
+        <ul class="bg-red-500 h-min w-min flex">
+          <li><a class="pl-2 pr-2 cursor-pointer">Projects</a></li>
+          <li><a class="pl-2 pr-2 cursor-pointer">Resumé</a></li>
+          <li><a class="pl-2 pr-2 cursor-pointer">Contact</a></li>
         </ul>
-        <div class="w-10 h-10 bg-blue-500">
+        <div class="bg-blue-500 flex h-min w-20 object-right">
           <a href="https://github.com/actroian" target="_blank">
-            <img src="./github.png"/>
+            <img class="w-10 h-10" src="./github.png"/>
           </a>
           <a href="https://www.linkedin.com/in/adamtroiani" target="_blank">
-            <img src="./linkedin.png"/>
+            <img class="w-10 h-10" src="./linkedin.png"/>
           </a>
         </div>
       </div>
